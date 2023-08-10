0fa0e688ca990c83152aa6250bfbd90fd35ee8c7
Luis <LuisDEsquivel11@gmail.com>
2023-08-10 18:07:56 -0500

    Hacker rank question #5

--- js/hackerRankSolutions.js
+++ js/hackerRankSolutions.js
@@ -11,7 +11,7 @@ function simpleArraySum(ar) {
 
 }
 
-// 3. Alice and Bob compare 2 arrays and add a point to whoever has the greater value
+// 4. Alice and Bob compare 2 arrays and add a point to whoever has the greater value
 // index for index , if they are equal neither gets a point. Return both scores.
 function compareTriplets(a, b) {
     let aScore = 0;
@@ -30,3 +30,16 @@ function compareTriplets(a, b) {
     scores.push(bScore);
     return scores;
 }
+
+// 5. (Java)
+// In this challenge, you are required to calculate and print the sum of the elements in an
+// array, keeping in mind that some of those integers may be quite large.
+
+// public static long aVeryBigSum(List<Long> ar) {
+//     long total = 0;
+//     for (Long num : ar) {
+//         total += num;
+//     }
+//     return total;
+//
+// }
