e76bd0ea742c96dde8f54f3e140b59a3631cd7be
Luis <LuisDEsquivel11@gmail.com>
2023-08-27 12:44:42 -0500

    Find Digits solution for hacker rank

--- js/hackerRankSolutions.js
+++ js/hackerRankSolutions.js
@@ -405,4 +405,16 @@ function angryProfessor(k, a) {
     }
 }
 
+// Find Digits
+
+function findDigits(n) {
+    let count = 0;
+    let digits = n.toString().split('')
+    for (let i = 0; i < digits.length; i++) {
+        if (n % parseInt(digits[i]) === 0 ) {
+            count++
+        }
+    }
+    return count;
+}
 
